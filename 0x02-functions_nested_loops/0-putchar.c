f1502ce4d0adf2556df3cd33f60ee7cb90704d7a
ouissalouakrim <ouakrimouissal6@gmail.com>
2023-02-21 16:58:31 +0100

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * description: print putchar
+ * return:always 0
+ */
+int main(void)
+{
+	int i;
+	char x[] = "_putchar";
+
+	for (i = 0 ; i <= 7 ; i++)
+	{
+		_putchar(x[i]);
+		}
+_putchar('\n');
+
+
+
+return (0);
+}
