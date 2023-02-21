c21c5702f9121d1b602db149b12dab471ca5a249
ouissalouakrim <ouakrimouissal6@gmail.com>
2023-02-21 18:23:51 +0100

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * print_alphabet - utilizes on the _putchar function to print
+ * print_alphabet_x10 - utilizes on the _putchar function to print
  *                 the alphabet a - z 10 times
 */
 
