8b3f5cf322c4c23185ee898900a62d1937d8d74a
ouissalouakrim <ouakrimouissal6@gmail.com>
2023-02-23 17:04:49 +0100

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies int
+ * @a : first integer
+ * @b : second integer
+ * dicription: multplies two integers
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
