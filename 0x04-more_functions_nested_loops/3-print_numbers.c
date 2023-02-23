1d6a8a1bf6b7244b5358ca82c9441dd7dadbeab7
ouissalouakrim <ouakrimouissal6@gmail.com>
2023-02-24 00:28:26 +0100

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,19 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * print_numbers - prints numbers
- * dicription: print numbers from 0 to 9
- */
+ * print_numbers - print 0 - 9
+ *             only using _putchar twice
+ *
+ * Return: Always 0 (Success)
+*/
+
 void print_numbers(void)
 {
-	char i;
-	i = 0;
-	if (i >= 0 && i <= 9)
-	{
-		_putchar(i + '0');
-		_putchar('\n');
-	}
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
 }
